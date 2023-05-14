be585d0b6b2b2b229a8dd5d8ffae4b4c5af5cf71
Maleesha Kawshan <kawshanmaleesha0@gmail.com>
2023-05-14 12:09:25 +0530

    create room database

---
+++ app/src/main/java/com/example/easymeal/Meal.kt
@@ -0,0 +1,23 @@
+package com.example.easymeal
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+@Entity
+
+data class Meal(
+    @PrimaryKey (true)  val mealId:Int=0,
+    val Meal:String?,
+    val DrinkAlternate:String?,
+    val Category:String?,
+    val Area:String?,
+    val Instructions:String?,
+    val MealThumb:String?,
+    val Tags:String?,
+    val Youtube:String?,
+    val mealIngredients:String?,
+    val Measures:String?,
+    val Source:String?,
+    val ImageSource:String?,
+    val CreativeCommonsConfirmed:String?,
+    var Modifieddate:String?
+)
--- app/src/main/java/com/example/easymeal/SearchMealbyIngd.kt
+++ app/src/main/java/com/example/easymeal/SearchMealbyIngd.kt
@@ -1,11 +1,31 @@
 package com.example.easymeal
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SearchMealbyIngd : AppCompatActivity() {
+
+    private lateinit var retrieve: Button
+    private lateinit var saveMeal: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_mealby_ingd)
+        supportActionBar?.hide()
+
+        retrieve=findViewById(R.id.retbutton)
+        saveMeal=findViewById(R.id.savebutton)
+
+        retrieve.setOnClickListener {
+            if(textArea.text?.toString()!=null && textArea.text?.toString()!!.trim().isNotEmpty()){
+                textResults.setTextColor(Color.BLACK)
+                mutable2.clear()
+                bundleResult.clear()
+                getMealNameByIngredient(textArea.text.toString())
+                textResults.text=bundleResult
+            }
+        }
     }
 }
